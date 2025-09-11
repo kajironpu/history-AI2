55a289dd349366db736d3f6f08e3c8d15468b6c6
kajironpu <kajironpu@gmail.com>
2025-09-11 11:18:54 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -211,7 +211,13 @@ function checkAnswer(option, element, quiz) {
     document.getElementById('next-question-btn').style.display = 'block';
 
     //const result = option.isCorrect ? '正解！' : '不正解';
-    rationaleBox.innerHTML = `<strong>解説:</strong><br>${quiz.keyword_explanation}`;
+   rationaleBox.innerHTML = `
+<strong>解説:</strong><br>
+${quiz.keyword_explanation}<br>
+<strong>暗記ポイント:</strong><br>
+${quiz.keypoints}
+`;
+
 
     updateProgress();
 }
